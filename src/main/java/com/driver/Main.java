a52d73d2553a1f2c38d9f1750003c64d0baec71a
Kuldeep Kumar <kuldeepvyas267@gmail.com>
2024-07-16 21:23:28 +0530

    overriding complete

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+     A obj = new A();
+     System.out.println(obj.meth());
+
+     B obj1 = new B();
+        System.out.println(obj1.meth());
+
+    }
 }
