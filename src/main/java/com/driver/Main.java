c14a23de2e5892c6b1fffff5cc6ef8478c3a5738
Kuldeep Kumar <kuldeepvyas267@gmail.com>
2024-07-17 13:15:56 +0530

    overriding done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -13,8 +13,11 @@ class B extends A {
 public class Main {
     public static void main(String[] args){
 
-     A obj1 = new B();
-        System.out.println(obj1.meth());
+     A objA = new B();
+        System.out.println(objA.meth());
+
+     B objB = new B();
+        System.out.println(objB.meth());
 
     }
 }
